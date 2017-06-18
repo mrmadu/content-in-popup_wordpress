1aea96e4cd6f8642824e9dfec173ca084b8a0d23
Max Dudin <mr.madu@ya.ru>
2017-06-18 12:52:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<a href="#" id="<?php the_ID(); ?>" class="ajax-post hvr-bounce-to-bottom m1" data-toggle="modal" data-target=".read-more"><?php the_title(); ?></a>
+
+<div class="modal fade read-more" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div id="ajax-response" style="padding: 15px;">
+				</div>
+			</div>
+		</div>
+	</div>
